d72b7c1f0d1808af0eda3f94d49bc134cd420b9c
Maksim Melnikau <maxposedon@gmail.com>
2014-04-20 22:53:39 +0300

    don't thread touch events as mouse events
    Signed-off-by: Maksim Melnikau <maxposedon@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_SynthesizeMouseForUnhandledTouchEvents, false);
     QStringList args = app.arguments();
     int zoom = args.size() > 1 ? args.at(1).toInt() : 0;
     int x = args.size() > 3 ? args.at(2).toInt() : 0;
