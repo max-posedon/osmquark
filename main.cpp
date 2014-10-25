3bf775a712aaea42f4b78a1193e5e836cab42076
Maksim Melnikau <maxposedon@gmail.com>
2014-10-25 17:34:27 +0300

    add project and organization information

--- main.cpp
+++ main.cpp
@@ -6,12 +6,21 @@
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+    app.setApplicationName("osmquark");
+    app.setApplicationVersion("1.0");
+    app.setOrganizationName("max_posedon");
+    app.setOrganizationDomain("maxposedon.org");
+
     app.setAttribute(Qt::AA_SynthesizeMouseForUnhandledTouchEvents, false);
+
     QStringList args = app.arguments();
     int zoom = args.size() > 1 ? args.at(1).toInt() : 0;
     int x = args.size() > 3 ? args.at(2).toInt() : 0;
     int y = args.size() > 3 ? args.at(3).toInt() : 0;
+
     OsmWindow window(0, zoom, x, y);
     window.show();
+
     return app.exec();
 }
