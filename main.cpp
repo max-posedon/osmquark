916ed974ae7124b409cbb46086c158c9827d27ef
Maksim Melnikau <maxposedon@gmail.com>
2014-10-26 18:16:07 +0300

    use QCommandLineParser to parse arguments
    Signed-off-by: Maksim Melnikau <maxposedon@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QtCore/QTimer>
+#include <QtCore/QCommandLineParser>
 #include <QtWidgets/QApplication>
 
 #include "osmwindow.h"
@@ -7,17 +8,29 @@ int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    QApplication::setApplicationName(APP_NAME);
-    QApplication::setApplicationVersion(APP_VERSION);
-    QApplication::setOrganizationName("max_posedon");
-    QApplication::setOrganizationDomain("maxposedon.org");
+    QCoreApplication::setApplicationName(APP_NAME);
+    QCoreApplication::setApplicationVersion(APP_VERSION);
+    QCoreApplication::setOrganizationName("max_posedon");
+    QCoreApplication::setOrganizationDomain("maxposedon.org");
 
-    QApplication::setAttribute(Qt::AA_SynthesizeMouseForUnhandledTouchEvents, false);
+    QCoreApplication::setAttribute(Qt::AA_SynthesizeMouseForUnhandledTouchEvents, false);
 
-    QStringList args = app.arguments();
-    int zoom = args.size() > 1 ? args.at(1).toInt() : 0;
-    int x = args.size() > 3 ? args.at(2).toInt() : 0;
-    int y = args.size() > 3 ? args.at(3).toInt() : 0;
+    QCommandLineOption zoomOption(QStringList() << "z" << "zoom", QObject::tr("Zoom Level"), "zoom", "0");
+    QCommandLineOption xOption("x", QObject::tr("main", "X offset"), "x", "0");
+    QCommandLineOption yOption("y", QObject::tr("main", "Y offset"), "y", "0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("very simple open street map app");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOption(zoomOption);
+    parser.addOption(xOption);
+    parser.addOption(yOption);
+    parser.process(app);
+
+    int zoom = parser.value(zoomOption).toInt();
+    int x = parser.value(xOption).toInt();
+    int y = parser.value(yOption).toInt();
 
     OsmWindow window(0, zoom, x, y);
     window.show();
